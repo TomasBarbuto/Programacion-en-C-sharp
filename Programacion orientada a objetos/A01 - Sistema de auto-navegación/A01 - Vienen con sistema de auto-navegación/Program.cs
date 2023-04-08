7ae7d9e6948c714870b331868200afb47c39cbca
Tomas Barbuto <barbutotomas@gmail.com>
2023-04-08 20:01:54 -0300

    ListoPorHoy

---
+++ Programacion orientada a objetos/A01 - Sistema de auto-navegación/A01 - Vienen con sistema de auto-navegación/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace A01___Vienen_con_sistema_de_auto_navegación
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Programacion orientada a objetos/A01 - Sistema de auto-navegación/Entidades/Conductor.cs
@@ -0,0 +1,14 @@
+﻿namespace Entidades
+{
+    public class Conductor
+    {
+        private string nombre;
+        private int[] kmPorDia;
+
+        public Conductor(string nombre, int[] kmPorDia)
+        {
+            this.nombre = nombre;
+            this.kmPorDia = kmPorDia;
+        }
+    }
+}
