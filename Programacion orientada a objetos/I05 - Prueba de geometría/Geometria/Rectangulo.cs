d95629dd07e603583db78ffdd770572ae143bdd9
Tomas Barbuto <barbutotomas@gmail.com>
2023-04-08 19:35:36 -0300

    PruebaGeometria

--- Programacion orientada a objetos/I05 - Prueba de geometría/Geometria/Rectangulo.cs
+++ Programacion orientada a objetos/I05 - Prueba de geometría/Geometria/Rectangulo.cs
@@ -13,16 +13,30 @@
         {
             this.vertice1 = vertice1;
             this.vertice3 = vertice3;
+
+            float baseRectangulo = Math.Abs(vertice3.GetX() - vertice1.GetX());
+            float alturaRectangulo = Math.Abs(vertice3.GetY() - vertice1.GetY());
+
+            this.vertice2 = new Punto(vertice3.GetX(), vertice1.GetY());
+            this.vertice4 = new Punto(vertice1.GetX(), vertice3.GetY());
+
+            Console.WriteLine(baseRectangulo);
+            Console.WriteLine(alturaRectangulo);
+            this.area = baseRectangulo * alturaRectangulo;
+
+            this.perimetro = (2 * baseRectangulo) + (2 * alturaRectangulo);
         }
 
-        public float Area()
+        public float GetArea()
         {
             return area;
         }
 
-        public float Perimetro() 
+        public float GetPerimetro() 
         {
-            
+            return perimetro;
         }
+
+
     }
 }
--- Programacion orientada a objetos/I05 - Prueba de geometría/I05 - Prueba de geometría/Program.cs
+++ Programacion orientada a objetos/I05 - Prueba de geometría/I05 - Prueba de geometría/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace I05___Prueba_de_geometría
+﻿using Geometria;
+namespace I05___Prueba_de_geometría
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Punto vertice1 = new Punto(10, 5);
+            Punto vertice3 = new Punto(20, 10);
+            Rectangulo rectangulo = new Rectangulo(vertice1, vertice3);
+
+            Console.WriteLine("Datos del rectangulo:");
+            Console.WriteLine("Área: {0}", rectangulo.GetArea());
+            Console.WriteLine("Perímetro: {0}", rectangulo.GetPerimetro());
         }
     }
 }
