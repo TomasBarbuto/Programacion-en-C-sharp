b66f9666c4a03d69ca9681330e109ca83a83ea83
Tomas Barbuto <barbutotomas@gmail.com>
2023-04-08 18:54:48 -0300

    PCConInventoArgentino

--- Programacion orientada a objetos/I04 - Invento argentino/Entidades/Boligrafo.cs
+++ Programacion orientada a objetos/I04 - Invento argentino/Entidades/Boligrafo.cs
@@ -6,11 +6,48 @@
         private ConsoleColor color;
         private short tinta;
 
-        public Boligrafo(short tinta, ConsoleColor color)
+        public Boligrafo(ConsoleColor color, short tinta)
         {
-
+            this.color = color;
+            this.tinta = tinta;
         }
 
-        public ConsoleColor 
+        public ConsoleColor GetColor()
+        {
+            return color;
+        }
+
+        public short GetTinta()
+        {
+            return tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            short nivelTinta = (short)(this.tinta + tinta);
+
+            if (nivelTinta >= 0 && nivelTinta <= cantidadTintaMaxima)
+            {
+                this.tinta = nivelTinta;
+            }
+            else
+            {
+                Console.WriteLine("No se pudo realizar la carga\n");
+            }
+        }
+
+        public void Recargar()
+        {
+            SetTinta((short)(cantidadTintaMaxima - tinta));
+        }
+
+        public string Pintar(short gasto, string dibujo)
+        {
+
+            short tintaGastada = Math.Min(gasto, tinta);
+            SetTinta((short)-tintaGastada);
+            Console.ForegroundColor = color;
+            return new string('*', tintaGastada) + dibujo;
+        }
     }
 }
--- Programacion orientada a objetos/I04 - Invento argentino/I04 - Invento argentino/Program.cs
+++ Programacion orientada a objetos/I04 - Invento argentino/I04 - Invento argentino/Program.cs
@@ -1,11 +1,41 @@
-﻿namespace I04___Invento_argentino
+﻿using Entidades;
+
+namespace I04___Invento_argentino
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-           
+            Boligrafo boligrafoAzul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo boligrafoRojo = new Boligrafo(ConsoleColor.Red, 50);
+
+            Console.WriteLine($"Boligrafo azul: color={boligrafoAzul.GetColor()}, tinta={boligrafoAzul.GetTinta()}");
+            Console.WriteLine($"Boligrafo rojo: color={boligrafoRojo.GetColor()}, tinta={boligrafoRojo.GetTinta()}");
+
+            Console.WriteLine("Recargando boli azul\n");
+            boligrafoAzul.Recargar();
+            Console.WriteLine($"color={boligrafoAzul.GetColor()}, tinta={boligrafoAzul.GetTinta()}\n");
+
+            Console.WriteLine("Recargando boli Rojo\n");
+            boligrafoRojo.Recargar();
+            Console.WriteLine($"color={boligrafoRojo.GetColor()}, tinta={boligrafoRojo.GetTinta()}\n");
+
+            string resultado = boligrafoRojo.Pintar(5, "*");
+            Console.WriteLine($"5 unidades con boligrafo rojo...");
+            Console.WriteLine($"Resultado: {resultado}, tinta restante: {boligrafoRojo.GetTinta()}");
+
+            resultado = boligrafoAzul.Pintar(10, "*");
+            Console.WriteLine($"10 unidades con boligrafo azul...");
+            Console.WriteLine($"Resultado: {resultado}, tinta restante: {boligrafoAzul.GetTinta()}");
+
+            resultado = boligrafoRojo.Pintar(7, "*");
+            Console.WriteLine($"Intentando pintar 100 unidades con boligrafo rojo...");
+            Console.WriteLine($"Resultado: {resultado}, tinta restante: {boligrafoRojo.GetTinta()}");
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
